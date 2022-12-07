9b834ff53a27afd4126ceffc7d9c844822132dff
Andrew Delph <andrewdelph@live.ca>
2022-12-07 15:44:06 -0500

    with cluster name find the public ip of task on it

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import express from "express";
 import * as AWS from "aws-sdk";
-import { ListClustersResponse, ListTasksResponse } from "aws-sdk/clients/ecs";
-import { AWSError } from "aws-sdk";
-import { PromiseResult } from "aws-sdk/lib/request";
 const app = express();
 const port = 5000;
 
@@ -18,48 +15,45 @@ const ecs = new AWS.ECS({
   region: "us-east-1",
 });
 
+const ec2 = new AWS.EC2({
+  region: "us-east-1",
+});
+
 const clusterName = "game-cluster";
 
 var params = {
   cluster: clusterName,
 };
 
-// ecs
-//   .listTasks(params)
-//   .promise()
-//   .then((value) => {
-//     var params = {
-//       cluster: clusterName,
-//       tasks: [value.taskArns[0]],
-//     };
-//     return ecs.describeTasks(params).promise();
-//   })
-//   .then((value) => {
-//     const task = value.tasks[0];
-
-//     const attachments = task.attachments;
-
-//     const elasticNetworkInterface = attachments[0];
-
-//     console.log("elasticNetworkInterface", elasticNetworkInterface);
-//   })
-//   .catch((rejection) => {
-//     console.error(rejection);
-//   });
-
-const ec2 = new AWS.EC2({
-  region: "us-east-1",
-});
-
-const paramsENI = {
-  NetworkInterfaceIds: ["eni-08c2866d98b1f41a6"],
-};
-
-ec2
-  .describeNetworkInterfaces(paramsENI)
+ecs
+  .listTasks(params)
   .promise()
   .then((value) => {
-    const first = value.NetworkInterfaces[0];
+    var params = {
+      cluster: clusterName,
+      tasks: [value.taskArns[0]],
+    };
+    return ecs.describeTasks(params).promise();
+  })
+  .then((value) => {
+    const task = value.tasks[0];
+    const attachments = task.attachments;
+    const elasticNetworkInterface = attachments[0];
 
+    const networkInterfaceId = elasticNetworkInterface.details.find(
+      (element) => element.name == "networkInterfaceId"
+    );
+
+    const params = {
+      NetworkInterfaceIds: [networkInterfaceId.value],
+    };
+
+    return ec2.describeNetworkInterfaces(params).promise();
+  })
+  .then((value) => {
+    const first = value.NetworkInterfaces[0];
     console.log("PublicIp", first.Association.PublicIp);
+  })
+  .catch((rejection) => {
+    console.error(rejection);
   });
