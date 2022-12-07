ef302fca8e87486be7012bde63d557c797b6a1cc
Andrew Delph <andrewdelph@live.ca>
2022-12-07 10:52:42 -0500

    init xpress

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+const port = 5000;
+app.get("/", (_, res) => {
+  res.status(200).send("hello1");
+});
+
+app.listen(port, () => console.log(`Running on port ${port}`));
+console.log("hi");
